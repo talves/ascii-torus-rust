ecbdda2b2e33a6696d4398d122fd3dafabeec04c
talves <tony@3alves.com>
2023-10-30 14:13:19 -0700

    update: donut

--- src/main.rs
+++ src/main.rs
@@ -27,44 +27,57 @@ fn rotation(tan0: f32, mut x: f32, mut y: f32) -> (f32, f32) {
 }
 
 fn main() {
-    let mut sin_a: f32 = 0.;
-    let mut cos_a: f32 = 1.;
-    let mut sin_b: f32 = 1.;
-    let mut cos_b: f32 = 0.;
+    let (mut cos_a, mut sin_a): (f32, f32) = (0., 1.);
+    let (mut cos_b, mut sin_b): (f32, f32) = (1., 0.);
     loop {
-        let mut z: [f32; SCALE_SIZE] = [0.; SCALE_SIZE]; // z buffer
-        let mut b: [char; SCREEN_SIZE] = [' '; SCREEN_SIZE]; // char buffer
-        let (mut cos_theta, mut sin_theta): (f32, f32) = (1., 0.);
-        for _i in 0..90 {
-            let (mut cos_phi, mut sin_phi): (f32, f32) = (1., 0.);
-            for _j in 0..314 {
-                // the x,y coordinate of the circle, before revolving
-                let circle_x: f32 = cos_theta + 2.;
-                let circle_y: f32 = sin_theta * 1.;
-                // One over z = (sin_phi * circle_x * sin_a + circle_y * cos_a + 5.)
-                let ooz: f32 = 1. / (sin_phi * circle_x * sin_a + circle_y * cos_a + 5.);
-                let t: f32 = (sin_phi * circle_x * cos_a) - (circle_y * sin_a);
-                let x: i32 = ((CHAR_WIDTH / 2) as f32 + (VIEW_X_POS * ooz * ((cos_phi * circle_x * cos_b) - (t * sin_b)))).round() as i32;
-                let y: i32 = (11. + (VIEW_Y_POS * ooz * ((cos_phi * circle_x * sin_b) + (t * cos_b)))).round() as i32;
-                let o: usize = (x + (CHAR_WIDTH * y)) as usize;
-                let tan_dist: usize = (8.
-                    * ((((sin_theta * sin_a) - (sin_phi * cos_theta * cos_a)) * cos_b)
-                        - (sin_phi * cos_theta * sin_a)
-                        - (sin_theta * cos_a)
-                        - (cos_phi * cos_theta * sin_b))) as usize;
-                if 0 < y && y < 22 && 0 < x && x < CHAR_WIDTH && ooz > z[o] {
-                    z[o] = ooz;
-                    b[o] = char::from(b".,-~:;=!*#$@"[if tan_dist > 0 { tan_dist } else { 0 }]);
+        let mut loc_z: [f32; SCALE_SIZE] = [0.; SCALE_SIZE];
+        let mut tan_char: [char; SCREEN_SIZE] = [' '; SCREEN_SIZE];
+        let (mut cos_theta, mut sin_theta): (f32, f32) = (0., 1.0);
+        for _theta in 0..90 {
+            let (mut cos_phi, mut sin_phi): (f32, f32) = (0., 1.0);
+            let mut circle_x: f32;
+            let mut z: f32;
+            let mut ooz: f32;
+            let mut t: f32;
+            let mut x: i32;
+            let mut y: i32;
+            let mut o: usize;
+            let mut tan_dist: usize;
+            for _phi in 0..314 {
+                circle_x = sin_theta + 2.;
+                z = cos_phi * circle_x * cos_a + cos_theta * sin_a + 5.;
+                ooz = 1. / z;
+                t = (cos_phi * circle_x * sin_a) - (cos_theta * cos_a);
+                x = ((CHAR_WIDTH / 2) as f32
+                    + (VIEW_X_POS * ooz * (sin_phi * circle_x * sin_b - t * cos_b)))
+                    as i32;
+                y = (11.0 + (VIEW_Y_POS * ooz * (sin_phi * circle_x * cos_b + t * sin_b))) as i32;
+                o = (x + (CHAR_WIDTH * y)) as usize;
+                tan_dist = (8.
+                    * (((cos_theta * cos_a - cos_phi * sin_theta * sin_a) * sin_b)
+                        - cos_phi * sin_theta * cos_a
+                        - cos_theta * sin_a
+                        - sin_phi * sin_theta * cos_b)) as usize;
+                if 0 < y && y < 22 && 0 < x && x < CHAR_WIDTH && ooz > loc_z[o] {
+                    loc_z[o] = ooz;
+                    tan_char[o] = b".,-~:;=!*#$@"[if tan_dist > 0 { tan_dist } else { 0 }] as char
                 }
-                (cos_phi, sin_phi) = rotation(PHI_SPACING, cos_phi, sin_phi);
+                (sin_phi, cos_phi) = rotation(0.02, sin_phi, cos_phi);
             }
-            (cos_theta, sin_theta) = rotation(THETA_SPACING, cos_theta, sin_theta);
+            (sin_theta, cos_theta) = rotation(0.07, sin_theta, cos_theta);
         }
         for k in 0..=SCREEN_SIZE {
-            print!("{}", if k % CHAR_WIDTH as usize != 0 { b[k] } else { '\n' });
+            print!(
+                "{}",
+                if k % CHAR_WIDTH as usize != 0 {
+                    tan_char[k]
+                } else {
+                    '\n' as char
+                }
+            )
         }
-        (cos_a, sin_a) = rotation(0.04, cos_a, sin_a);
-        (cos_b, sin_b) = rotation(0.02, cos_b, sin_b);
+        (sin_a, cos_a) = rotation(THETA_SPACING, sin_a, cos_a);
+        (sin_b, cos_b) = rotation(PHI_SPACING, sin_b, cos_b);
         std::thread::sleep(std::time::Duration::from_millis(15));
 
         print!(" donut.rs! \x1b[23A");
